61fccc5fa8a4fc4f7595555820c021053a99bda0
allvinnik <allvinnik@mail.ru>
2022-09-02 20:51:18 +0300

    proverka env

--- backend/configuration/index.js
+++ backend/configuration/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { PORT = 3000, JWT_SECRET = '1b15f173030bcb73df6e84f48acfdb3cb2dc402941eb30e634aa30fdd8bd5fdd' } = process.env;
+const { PORT = 3000, JWT_SECRET = 'b15f173030bcb73df6e84f48acfdb3cb2dc402941eb30e634aa30fdd8bd5fdd' } = process.env;
 
 const DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
 const JWT_STORAGE_TIME = '7d';
